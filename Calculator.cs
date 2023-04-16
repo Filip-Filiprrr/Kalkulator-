3874f0764d465c8b3f8685d28c41866970d4fca2
Filip Wroński <79693782+Filip-Filiprrr@users.noreply.github.com>
2023-04-16 16:38:56 +0200

    Add files via upload

--- Calculator.cs
+++ Calculator.cs
@@ -11,13 +11,13 @@ namespace app
             double total = 0;
             for (int i = 0; i < znaki.Length; i++)
             {
-                (double, int, bool) wynik = Calculate(exp, i);
-                
+                (double, int, bool, int, int) wynik = Calculate(exp, i);
+
                 if (wynik.Item3)
                 {
-                    exp = SubString(exp, wynik.Item1, wynik.Item2);
+                    exp = SubString(exp, wynik.Item1, wynik.Item2, wynik.Item4, wynik.Item5);
                     total += wynik.Item1;
-                    i=0;
+                    i = 0;
                 }
             }
 
@@ -25,18 +25,31 @@ namespace app
 
         }
 
-        private string SubString(string exp, double wynik, int j)
+        private string SubString(string exp, double wynik, int j, int L, int R)
         {
             string sleft = "", sright = "";
 
             if (j - 1 > 0)
             {
-                sleft = exp.Substring(0, j - 1);
+                sleft = exp.Substring(0, j - L);
             }
 
             if (j + 2 < exp.Length - 1)
             {
-                sright = exp.Substring(j + 2, exp.Length - j - 2);
+                sright = exp.Substring(j + R, exp.Length - j - R);
+            }
+
+
+
+            if (isLeftMinus(sleft) && isWynikMinus(wynik))
+            {
+                sleft = sleft.Remove(sleft.Length - 1) + "+";
+
+            }
+
+            if(isLeftPlus(sleft))
+            {
+                sleft = sleft.Remove(sleft.Length - 1);
             }
 
             exp = sleft + wynik.ToString() + sright;
@@ -47,43 +60,186 @@ namespace app
 
         }
 
-        private (double, int, bool) Calculate(string exp, int i)
+        private (double, int, bool, int, int) Calculate(string exp, int i)
         {
 
-            double left, right;
+            int L = 0;
+            int R = 0;
 
             for (int j = 0; j < exp.Length; j++)
             {
 
-                if (exp[j] == znaki[i])
+                if (exp[j] == znaki[i] && j >= 1)
                 {
-                    left = Double.Parse(exp[j - 1].ToString());
-                    right = Double.Parse(exp[j + 1].ToString());
+                    (double, int) left = NumbersToLeft(exp, j - 1);
+                    (double, int) right = NumbersToRight(exp, j + 1);
+
+                    L = left.Item1.ToString().Length;
+                    R = right.Item1.ToString().Length;
+
+                    (int, double, string) minus = NegativeNumberLeft(j, left.Item1, exp);
+                    left.Item1 = minus.Item2;
+
+                    if (left.Item1 < 0)
+                    {
+                        R++;
+                    }
 
                     switch (exp[j].ToString())
                     {
                         case "*":
 
-                            return (left * right, j, true);
+                            return (left.Item1 * right.Item1, j, true, L, R);
 
                         case "/":
 
-                            return (left / right, j, true);
+                            return (left.Item1 / right.Item1, j, true, L, R);
 
                         case "+":
 
-                            return (left + right, j, true);
+                            return (left.Item1 + right.Item1, j, true, L, R);
 
                         case "-":
 
-                            return (left - right, j, true);
-
+                            return (left.Item1 - right.Item1, j, true, L, R);
 
                     }
                 }
             }
-            
-            return (0, j, false);
+
+            return (0, j, false, L, R);
+        }
+
+        public (double, int) NumbersToLeft(string exp, int startIndex)
+        {
+            string N = "";
+
+            int i = startIndex;
+
+            for (; i >= 0; i--)
+            {
+                if (!isMarkLeft(exp[i]))
+                {
+                    N += exp[i];
+                }
+                else
+                {
+                    break;
+                }
+            }
+            string N2 = N;
+            char[] zmieniony = N2.ToCharArray();
+            Array.Reverse(zmieniony);
+            string reversedStr = new string(zmieniony);
+
+            return (double.Parse(reversedStr!), i);
+        }
+
+        public bool isMarkLeft(char c)
+        {
+            for (int i = 0; i < znaki.Length; i++)
+            {
+                if (znaki[i] == c)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public (double, int) NumbersToRight(string exp, int startIndex)
+        {
+            string M = "";
+
+            int i = startIndex;
+
+            for (; i < exp.Length; i++)
+            {
+                if (!isMarkRight(exp[i]))
+                {
+                    M += exp[i];
+                }
+                else
+                {
+                    break;
+                }
+            }
+            string? M2 = M.ToString();
+
+            return (double.Parse(M2!), i);
+        }
+
+        public bool isMarkRight(char d)
+        {
+            for (int i = 0; i < znaki.Length; i++)
+            {
+                if (znaki[i] == d)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public (int, double, string) NegativeNumberLeft(int znak, double left, string exp)
+        {
+            if (znak - left.ToString().Length - 1 >= 0)
+                if (isMinus(exp[znak - left.ToString().Length - 1]))
+                {
+                    left = Double.Parse("-" + left.ToString());
+                }
+
+            return (znak, left, exp);
+
+        }
+
+        public bool isMinus(char e)
+        {
+
+            if (e.ToString() == "-")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool isLeftMinus(string f)
+        {
+            if (f == "")
+            {
+                return false;
+            }
+            if (f[f.Length - 1].ToString() == "-")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool isLeftPlus(string f)
+        {
+            if (f == "")
+            {
+                return false;
+            }
+            if (f[f.Length - 1].ToString() == "+")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool isWynikMinus(double wynik)
+        {
+            if (wynik < 0)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
