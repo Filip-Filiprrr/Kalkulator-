41dd5314cdbe1ea734954803410be3142318ad2c
Filip Wroński <79693782+Filip-Filiprrr@users.noreply.github.com>
2023-04-23 11:10:22 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,15 +4,38 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var kalkulator = new Calculator();
+        // var calculator = new Calculator();
+
+        // System.Console.WriteLine("Wpisz dzialanie:");
         
-        string? dzialanie = "2-2";
+        // string? operation = "2+2*3+4/2";
         
-        double a = 0;
+        // double a = 0;
         
-        if (dzialanie != null)
+        // if (operation != null)
+        // {
+        //     a = calculator.Execute(operation);
+        // }
+
+        // System.Console.WriteLine(a);
+
+        string exp = "7+2*4";
+
+        var p = new Parser(exp);
+        
+        var l = p.ReadAll();
+
+        foreach(var t in l)
         {
-            a = kalkulator.Execute(dzialanie);
+            System.Console.WriteLine("{0} - {1}", t.Value(), t.TokenType());
         }
+
+        // var r = new Reader(exp);
+
+        // while(r.MoveNext())
+        // {
+        //     var t = r.Current;
+        //     System.Console.WriteLine("{0} - {1}", t.Value(), t.TokenType());
+        // }
     }
 }
