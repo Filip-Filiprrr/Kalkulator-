3da80be8bb5ade24e8cb61ec9face13b69b2b1b3
Filip Wronski <filipwronski@FilipMacAir.lan>
2023-09-17 22:08:22 +0200

    test

--- kalkulator/src/app/Program.cs
+++ kalkulator/src/app/Program.cs
@@ -4,15 +4,44 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var kalkulator = new Calculator();
+        //var calculator = new Calculator();
+
+        // System.Console.WriteLine("Wpisz dzialanie:");
         
-        string? dzialanie = Console.ReadLine();
+        // string? operation = "((7-2)+16)";
         
-        double a = 0;
+        // double a = 0;
         
-        if (dzialanie != null)
-        {
-            a = kalkulator.Execute(dzialanie);
-        }
+        // if (operation != null)
+        // {
+        //     a = calculator.Execute(operation);
+        // }
+
+        // System.Console.WriteLine(a);
+
+        string exp = "(12+34)*56";
+
+        var p = new Parser(exp);
+        
+        var l = p.ReadAll();
+        //test
+        var root = new TreeBuilder().Build(l);
+
+        var wynik = new TreeKalkulator(new DecimalVisitor()).Execute(root);
+
+        System.Console.WriteLine(wynik);
+
+        // foreach(var t in l)
+        // {
+        //     System.Console.WriteLine("{0} - {1}", t.Value(), t.TokenType());
+        // }
+
+        // var r = new Reader(exp);
+
+        // while(r.MoveNext())
+        // {
+        //     var t = r.Current;
+        //     System.Console.WriteLine("{0} - {1}", t.Value(), t.TokenType());
+        // }
     }
 }
